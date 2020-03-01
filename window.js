1e3097b7fb33620ca74a8ef73300e8b06554b49c
Elwardi <elwardifadeli@gmail.com>
2020-03-01 10:25:08 +0100

    ASync editor-terminal function run in order

--- window.js
+++ window.js
@@ -8,48 +8,78 @@ $(() => {
     const yaml = require('js-yaml');
     const hotkeys = require('hotkeys-js');
     const Prism = require('prismjs');
+    const async = require('async');
+    const async_series = require('async-series');
 
     let nonce = crypto.randomBytes(16).toString('base64');
 
     // Convenience functions
     // Show a piece of code
-    function showCode(container, filepath) {
-        const demo = new Gdemo(container)
+    async function showCode(container, filepath) {
         let fileContents = fs.readFileSync(filepath, 'utf-8');
         let data = yaml.safeLoad(fileContents);
 
-        // Render text in editor-like env
-        if (Object.keys(data).includes('editor'))
-        {
-            data.editor.forEach( function(item, index) {
-                if (item.language == 'cpp')
-                {
-                    const preLng = require('prismjs/components/prism-c');
-                }
-                const lng = require('prismjs/components/prism-'+item.language);
-                const highlightedCode = Prism.highlight(
-                    item.text,
-                    Prism.languages[item.language],
-                    item.language
-                );
-                demo.openApp('editor', item.options).write(highlightedCode).end();
-            });
+        var completededitors = 0;
+        var completededitorsCached = completededitors;
+
+        function renderEditors() {
+            if (Object.keys(data).includes('editor'))
+            {
+                async.eachSeries(
+                    data.editor,
+                    async function(item, next) {
+                        const demo = new Gdemo(container)
+                        if (item.language == 'cpp')
+                        {
+                            const preLng = require('prismjs/components/prism-c');
+                        }
+                        const lng = require('prismjs/components/prism-'+item.language);
+                        const highlightedCode = Prism.highlight(
+                            item.text,
+                            Prism.languages[item.language],
+                            item.language
+                        );
+                        await demo.openApp('editor', item.options).write(highlightedCode);
+                        await demo.end();
+                }, function(){completededitors = 1;});
+            }
         }
-        // Render terminal commands
-        if (Object.keys(data).includes('terminals'))
-        {
-            data.terminals.forEach( function(item, index) {
-                demo.openApp('terminal', item.options)
-                item.commands.forEach(com => {
-                    demo.command(com.command, com.options);
-                    if (Object.keys(com).includes('output'))
-                    {
-                        demo.respond(com.output);
+
+        function renderTerminals() {
+            if (!Object.keys(data).includes('editor'))
+            {
+                completededitors = 1;
+            }
+            if(completededitors === completededitorsCached)
+            {
+                console.log("stepping")
+                setTimeout(renderTerminals, 1000);
+                return;
+            }
+            completededitorsCached = completededitors;
+            if (Object.keys(data).includes('terminals'))
+            {
+                async.eachSeries(
+                    data.terminals,
+                    async function(item, next) {
+                        const demo = new Gdemo(container);
+
+                        await demo.openApp('terminal', item.options)
+                        item.commands.forEach(com => {
+                            demo.command(com.command, com.options);
+                            if (Object.keys(com).includes('output'))
+                            {
+                                demo.respond(com.output);
+                            }
+                        });
+                        await demo.end();
                     }
-                });
-            });
+                );
+            }
         }
-        demo.end();
+
+        renderEditors();
+        renderTerminals();
     }
 
     // Toggle Input field display
@@ -72,7 +102,9 @@ $(() => {
     let fileContents = fs.readFileSync('database.json');
     let db = JSON.parse(fileContents);
     var fuzzyOptions = {
-        keys: ['description','title','path']
+        keys: ['description','title','path'],
+        threshold: 0.8,
+        minMatchCharLength: 5,
     }
 
     // Some vars to perform checks
@@ -83,19 +115,24 @@ $(() => {
     $('#text-input').on('input', function() {
         // Current input text
         const text = this.value
+        var displaying = false;
 
         // Delay searching until 3 chars are reached
-        if (text.length >= 3)
+        if (text.length >= 5)
         try {
-            // Actively fuzzy search in database
+            //Actively fuzzy search in database
             var f = new Fuse(Object.values(db), fuzzyOptions);
             var result = f.search(text);
 
             // Show code only if no
-            if ((Object.keys(result).length > 0) && (stk != result[0].path))
+            if (
+                (Object.keys(result).length > 0) 
+                && (stk != result[0].path)
+            )
             {
                 // If there is a result, use first match
                 $("#code-container").html("");
+                //var container = document.getElementById("code-container")
                 showCode("#code-container", result[0].path);
                 stk = result[0].path;
             }
